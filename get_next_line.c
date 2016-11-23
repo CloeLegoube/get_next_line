b560fe31b541e4bcee3c9bdd8052a3824169f499
Cloe LEGOUBE <clegoube@e2r8p12.42.fr>
2016-11-23 12:48:54 +0100

    Evolution de la fonction - phase de test

--- get_next_line.c
+++ get_next_line.c
@@ -1,18 +1,64 @@
 #include "get_next_line.h"
+#include <stdio.h>
 
-// typedef	struct	s_part
-// {
-// 	char			*content;
-// 	size_t			content_size;
-// 	struct s_part	*next;
-// }				t_part;
-
-int get_next_line(const int fd, char **line)
+static int		len_tab(char **tab)
 {
-	int		count_char;
+	int i;
+
+	i = 0;
+	while(tab[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+static int		ft_count_char(char **tab)
+{
 	int		i;
-	char	buff[BUFF_SIZE + 1];
-	static char *next_buff;
+	int		j;
+	int		count;
+
+	i = 1;
+	count = 0;
+	while (tab[i++])
+	{
+		j = 0;
+		while (tab[i][j++])
+			count++;
+		count++;
+	}
+	return (count);
+}
+
+char			*ft_concat_tab(char **tab)
+{
+	char	*string;
+	int		i;
+	int		j;
+	int		count;
+
+	count = ft_count_char(tab);
+	string = (char*)malloc((count + 1) * sizeof(*string));
+	i = 1;
+	count = 0;
+	while (tab[i++])
+	{
+		j = -1;
+		while (tab[i][++j])
+			string[count++] = tab[i][j];
+		string[count++] = i < len_tab(tab) ? '\n' : 0;
+	}
+	return (string);
+}
+
+int		get_next_line(const int fd, char **line)
+{
+	int			count_char;
+	int			i;
+	char		buff[BUFF_SIZE + 1];
+	static char	*next_buff;
+	char		**tab;
 
 	while ((count_char = read(fd, buff, BUFF_SIZE)))
 	{
@@ -22,11 +68,30 @@ int get_next_line(const int fd, char **line)
 			if (buff[i] == '\n')
 			{
 				if (next_buff)
-					*line = ft_strjoin(next_buff, ft_strsub(buff, 0, i));
+				{
+					tab = ft_strsplit(next_buff, '\n');
+					//ft_print_words_tables(tab);
+					if (len_tab(tab) > 1)
+					{
+						free(*line);
+						*line = NULL;
+						*line = (char *)malloc((ft_strlen(tab[0]) + 1) * sizeof(*line));
+						*line = tab[0];
+						free(next_buff);
+						next_buff = NULL;
+						next_buff = ft_concat_tab(tab);
+					}
+					else
+					{
+						free(*line);
+						*line = NULL;
+						*line = ft_strjoin(next_buff, ft_strsub(buff, 0, i));
+					}
+				}
 				else
 				{
 					*line = ft_strsub(buff, 0, i);
-					next_buff = ft_strsub(buff, i + 1, (count_char - i + 1));
+					next_buff = ft_strsub(buff, i + 1, (count_char - i - 1));
 				}
 				return (1);
 			}
@@ -35,9 +100,10 @@ int get_next_line(const int fd, char **line)
 				*line = ft_strsub(buff, 0, i);
 				return (0);
 			}
+			else
+				count_char++;
 			i++;
 		}
-
 	}
 	return (-1);
 }
@@ -78,13 +144,15 @@ int main(int argc, char **argv)
 		if (ft_open(fd) == 1)
 			return (1);
 		get_next_line(fd, line);
-		printf("%s", *line);
+		printf("%s\n", *line);
 		get_next_line(fd, line);
-		printf("%s", *line);
+		printf("%s\n", *line);
 		get_next_line(fd, line);
-		printf("%s", *line);
+		printf("%s\n", *line);
 		get_next_line(fd, line);
-		printf("%s", *line);
+		printf("%s\n", *line);
+		get_next_line(fd, line);
+		printf("%s\n", *line);
 		if (ft_close(fd) == 1)
 			return (1);
 	}
--- get_next_line.h
+++ get_next_line.h
@@ -7,11 +7,23 @@
 # include <stdio.h>
 # include <fcntl.h>
 
+typedef	struct	s_var
+{
+	int				chaine;
+	int				mot;
+	int				lettre;
+	struct s_var	*next;
+}				t_var;
+
 int			get_next_line(const int fd, char **line);
 char		*ft_strsub(char const *s, unsigned int start, size_t len);
 void		ft_putstr(char const *s);
 void		ft_putchar(char c);
 char		*ft_strjoin(char const *s1, char const *s2);
 size_t		ft_strlen(const char *s);
+char		**ft_strsplit(char const *str, char charset);
+char		*ft_concat_tab(char **tab);
+char		*ft_strcat(char *s1, const char *s2);
+void		ft_print_words_tables(char **tab);
 
 #endif
