ec09e4b99567d136e72acb85d7e9052b5a5b9be8
Cloe LEGOUBE <clegoube@e2r8p12.42.fr>
2016-11-27 17:41:24 +0100

    GNL version finale avant bonus - norminette OK- 27-11-2016 17h41

--- get_next_line.c
+++ get_next_line.c
@@ -1,152 +1,125 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: clegoube <clegoube@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/27 17:39:12 by clegoube          #+#    #+#             */
+/*   Updated: 2016/11/27 17:40:15 by clegoube         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 #include <stdio.h>
 
-void		ft_lstdelete(t_list *alst)
+int		if_stock_exist(t_list **stock, char **line)
 {
-	t_list 	*tmp;
+	int				j;
+	t_list			*new;
+	int				size;
 
-	if (alst->content != NULL && alst->content_size != 0)
+	j = 0;
+	while ((*(char *)((*stock)->content + j)) != '\0')
 	{
-		free(alst->content);
-		alst->content = NULL;
-		tmp = (alst)->next;
-		free(alst);
-		alst = tmp;
+		if ((*(char *)((*stock)->content + j)) == '\n')
+		{
+			ft_memdel((void *)line);
+			*line = ft_strsub((*stock)->content, 0, j);
+			size = (*stock)->content_size - j - 1;
+			new = ft_lstnew(ft_strsub((*stock)->content, j + 1, size), size);
+			ft_structdelete(*stock);
+			ft_lstadd(stock, new);
+			return (1);
+		}
+		j++;
 	}
+	return (0);
+}
+
+int		if_i_inf_ret(t_list **stock, char **line, char *buf, int ret)
+{
+	int				i;
+	t_list			*new;
+
+	i = -1;
+	while (++i < ret)
+	{
+		if (buf[i] == '\n' || buf[i] == '\0')
+		{
+			ft_memdel((void *)line);
+			if (*stock)
+			{
+				*line = ft_strnew(i + (*stock)->content_size);
+				*line = ft_strjoin((*stock)->content, ft_strsub(buf, 0, i));
+				new = ft_lstnew(ft_strsub(buf, i + 1, RET), RET);
+			}
+			else
+			{
+				*line = ft_strsub(buf, 0, i);
+				new = ft_lstnew(ft_strsub(buf, i + 1, RET), RET);
+			}
+			*stock = new;
+			return (-1);
+		}
+	}
+	return (i);
+}
+
+void	if_i_equal_ret(t_list **stock, char *buf, int ret, int i)
+{
+	t_list			*new;
+
+	new = NULL;
+	if (i == ret)
+	{
+		if (stock)
+			new = ft_lstnew(ft_strjoin((*stock)->content, ft_strsub(buf, 0, \
+				ft_strlen(buf))), ret + (*stock)->content_size);
+		else
+			new = ft_lstnew(buf, ret);
+		*stock = new;
+	}
+}
+
+int		if_ret_inf_size(t_list **stock, char **line, char *buf, int ret)
+{
+	if (ret < BUFF_SIZE)
+	{
+		if (*stock && ft_strchr((*stock)->content, '\n') != NULL)
+		{
+			ft_memdel((void *)line);
+			*line = ft_strjoin((*stock)->content, ft_strsub(buf, 0, ret));
+			return (0);
+		}
+	}
+	return (-1);
 }
 
 int		get_next_line(const int fd, char **line)
 {
 	int				ret;
 	int				i;
-	int				j;
 	char			*buf;
 	static t_list	*stock = NULL;
-	t_list			*new;
 	int				next_line;
 
 	ret = 0;
 	next_line = 0;
-	/*
-	** -----------------------------------------------------------------------------------------------
-	** 					VERIFICATION DE LA LISTE CHAINEE
-	** -----------------------------------------------------------------------------------------------
-	** Verifie si la liste chainee a stocke du buffer.
-	** Si oui, parcours le contenu et renvoie dans line la 1ere partie jusqu'a \n
-	** Le reste est de nouveau stocke dans la liste chaine au debut apres avoir supprime l'ancienne.
-	** Si aucun \n n'est trouve, on fait une nouvelle lecture.
-	*/
 	if (stock)
 	{
-		ft_putstr("test6");
-		j = 0;
-		while ((*(char *)(CONTENT + j)) != '\0')
-		{
-			if ((*(char *)(CONTENT + j)) == '\n')
-			{
-				ft_putstr("test8");
-				ft_memdel((void *)line);
-				//*line = ft_strnew(j);
-				*line = ft_strsub(CONTENT, 0, j);
-				new = ft_lstnew(ft_strsub(CONTENT, j + 1, (SIZE - j - 1)), (SIZE - j - 1));
-				ft_lstdelete(stock);
-				ft_lstadd(&stock, new);
-				return (1);
-			}
-			j++;
-		}
+		if (if_stock_exist(&stock, line) == 1)
+			return (1);
 	}
-	/*
-	** -----------------------------------------------------------------------------------------------
-	** 						LECTURE DU FICHIER
-	** -----------------------------------------------------------------------------------------------
-	** Si le nbre de charactere lu n'est pas null alors on parcours le buffer.
-	*/
 	buf = (char *)malloc((BUFF_SIZE + 1) * sizeof(buf));
 	while ((ret = read(fd, buf, BUFF_SIZE)))
 	{
-		ft_putstr("test1\n");
-		i = 0;
-		/*
-		** ---------------------------------------------------------------------------------------------------
-		** Si un \0 est trouve alors la lecture est terminee, on verifie si la liste chainee est vide ou non
-		** Si elle n'est pas vide, on renvoie line avec le restant de la liste et le debut de buffer jusqu'au 1er \n
-		** Il n'y a plus rien a stocker.
-		** Si elle est vide, on renvoie seulement le debut du buffer jusqu'au 1er \n
-		** Il n'y a plus rien a stocker.
-		*/
-		if (ret < BUFF_SIZE)
-		{
-			ft_putstr("test14");
-			if (stock && ft_strchr(CONTENT, '\n') != NULL)
-			{
-				ft_putstr("test11");
-				ft_memdel((void *)line);
-				//*line = ft_strnew(i + SIZE);
-				*line = ft_strjoin(CONTENT, ft_strsub(buf, 0, ret));
-				return (0);
-			}
-		}
-		while (i < ret)
-		{
-			printf("test 2 : %c --> %d\n", buf[i], i);
-
-			/*
-			** ---------------------------------------------------------------------------------------------------------
-			** Si un \n est trouve, on verifie si la liste chainee est vide ou non
-			** Si elle n'est pas vide, on renvoie line avec le restant de la liste et le debut de buffer jusqu'au 1er \n
-			** Le reste est stocke dans une liste chaine a la fin, apres avoir delete la struct precedente.
-			** Si elle est vide, on renvoie seulement le debut du buffer jusqu'au 1er \n
-			** et on stocke le reste dans la liste chainee a la fin.
-			*/
-			if (buf[i] == '\n' || buf[i] == '\0')
-			{
-				ft_putstr("test3");
-				if (stock != NULL && SIZE != 0)
-				{
-					ft_putstr("test4");
-					ft_memdel((void *)line);
-					//*line = ft_strnew(i + SIZE);
-					*line = ft_strjoin(CONTENT, ft_strsub(buf, 0, i));
-					new = ft_lstnew(ft_strsub(buf, i + 1, (ret - i - 1)), (ret - i - 1));
-					//ft_lstdelete(stock);
-					stock = new;
-				}
-				else
-				{
-					ft_putstr("test5");
-					ft_memdel((void *)line);
-					//*line = ft_strnew(i);
-					*line = ft_strsub(buf, 0, i);
-					new = ft_lstnew(ft_strsub(buf, i + 1, (ret - i - 1)), (ret - i - 1));
-					stock = new;
-				}
-				return (1);
-			}
-			i++;
-		}
-		/*
-		** ---------------------------------------------------------------------------
-		** Si le BUFF_SIZE < la longueur de la ligne.
-		** on stocke la valeur du buff ou on ajoute le buff dans un nouveau maillon
-		*/
-		if (i == ret)
-		{
-			ft_putstr("test13");
-			if (stock)
-			{
-				ft_putstr("test9");
-				new = ft_lstnew(ft_strjoin(CONTENT, buf), ret + SIZE);
-				ft_lstdelete(stock);
-				stock = new;
-			}
-			else
-			{
-				ft_putstr("test10");
-				new = ft_lstnew(buf, ret);
-				stock = new;
-			}
-		}
+		if (if_ret_inf_size(&stock, line, buf, ret) == 0)
+			return (0);
+		if (if_i_inf_ret(&stock, line, buf, ret) == -1 ||
+			((i = if_i_inf_ret(&stock, line, buf, ret)) && 0))
+			return (1);
+		if_i_equal_ret(&stock, buf, ret, i);
 	}
 	return (-1);
 }
