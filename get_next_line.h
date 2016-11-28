bc413c18afa548e8574683bcd3d200258c0f0e14
Cloe LEGOUBE <clegoube@e2r9p11.42.fr>
2016-11-28 16:19:36 +0100

    envoi du .h 28-11-2016 15h20

--- get_next_line.h
+++ get_next_line.h
@@ -1,26 +1,20 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 32
+# define BUFF_SIZE 100000
+# define SIZE (stock->content_size)
+# define CONTENT (stock->content)
+# define RET (ret - i - 1)
+# define T_SIZE (*SIZE - j - 1)
 
 # include <stdlib.h>
 # include <unistd.h>
 # include <stdio.h>
 # include <fcntl.h>
 
-typedef	struct	s_var
-{
-	int				chaine;
-	int				mot;
-	int				lettre;
-	struct s_var	*next;
-}				t_var;
-
 typedef	struct	s_buff
 {
-	char 			*new_buff;
-	char 			*line;
-	int				count_char;
-	struct s_buff	*next;
+	char 			*buffer;
+	int				fd;
 }				t_buff;
 
 typedef	struct	s_list
@@ -42,5 +36,13 @@ char		*ft_concat_tab(char **tab);
 char		*ft_strcat(char *s1, const char *s2);
 void		ft_print_words_tables(char **tab);
 void		ft_memdel(void **ap);
+t_list		*ft_lstnew(void const *content, size_t content_size);
+void		ft_lstadd_end(t_list **alst, t_list *new);
+char		*ft_strnew(size_t size);
+void		ft_lstdel(t_list **alst, void (*del)(void *, size_t));
+void		ft_lstadd(t_list **alst, t_list *new);
+void		ft_strdel(char **as);
+char		*ft_strchr(const char *s, int c);
+void		ft_structdelete(t_list *astruct);
 
 #endif
