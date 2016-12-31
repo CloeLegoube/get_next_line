84b7b221cc311d076d986d45186a9e1c235192e3
AC <AC@MacBook-Pro-de-AC.local>
2016-12-31 12:15:32 +0100

    envoi du fichier qui fonctionne avant passage au multi fd

--- get_next_line_fonctionne.c
+++ get_next_line_fonctionne.c
@@ -14,20 +14,14 @@
 #include "get_next_line.h"
 #include <stdio.h>
 
-
-void	line_and_stock(char *stock, char **line, char line_until, char *stock_until)
-{
-	*line = ft_strsub(stock, 0, line_until);
-	stock = ft_strcpy(stock, stock_until);
-}
-
 int		get_lines(char *stock, char **line)
 {
 	char			*tmp;
 
 	if ((tmp = ft_strchr(stock, '\n')))
 	{
-		line_and_stock(stock, line, (tmp - stock), (tmp + 1));
+		*line = ft_strsub(stock, 0, tmp - stock);
+		stock = ft_strcpy(stock, tmp + 1);
 		tmp = NULL;
 		return (1);
 	}
@@ -44,12 +38,14 @@ int		ret_inf_zero(char *stock, char **line, int ret)
 	{
 		if ((tmp = ft_strchr(stock, '\n')) != NULL) // ici tu verifie que ta lecture est terminee mais stock pas nulle
 		{
-			line_and_stock(stock, line, (tmp - stock), (tmp + 1));
+			*line = ft_strsub(stock, 0, (tmp - stock));
+			stock = ft_strcpy(stock, tmp + 1);
 			return (1); // au prochain appel, il ne peut pas y avoir un '/n' et un '/0'
 		}
 		if (tmp == NULL && ft_strcmp(stock, "\0") != 0) //ca gere le cas ou il n'y a pas de '/n' a la fin
 		{
-			line_and_stock(stock, line, (ft_strchr(stock, '\0') - stock), ft_strchr(stock, '\0'));
+			*line = ft_strsub(stock, 0, ft_strchr(stock, '\0') - stock);
+			stock = ft_strcpy(stock, ft_strchr(stock, '\0'));
 			return (1);
 		}
 		free(tmp);
